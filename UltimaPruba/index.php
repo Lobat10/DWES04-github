923adbe2b820e63183cedae5c3bc07521730b7e8
Alumno <Alumno@W7Bsica>
2017-10-17 14:22:16 +0200

    ABC

--- UltimaPruba/index.php
+++ UltimaPruba/index.php
@@ -1,3 +1,9 @@
 <?php
-echo "<h1>Funciona</h1>"
+echo "<h1>Funciona</h1>";
+    
+$nombre="Adrián";
+echo "Mi nombre es ". $nombre.".\n";
+
+define("ALUMNO", "Adrián Lobato");
+echo "El alumno es ".ALUMNO;
 ?>
